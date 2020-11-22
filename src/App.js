4d1647b7f019c87d341eb0e04d2580550785eb92
@Bharatsinghrajawat <bharatsingh1036@gmail.com>
2020-11-22 18:12:58 +0530

    hashroute added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Route , Switch} from 'react-router-dom';
+import {HashRouter as Router, Route , Switch} from 'react-router-dom';
 import home from './components/home';
 import about from './components/about';
 import contact from './components/contact'
